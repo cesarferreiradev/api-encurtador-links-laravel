8aebd8d6bfa96a734cf9c756661eabfa34812f84
César Ferreira <cesarferreiraa67@gmail.com>
2025-08-14 22:05:30 -0300

    feat: Retornando links

---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreLinkRequest;
+use App\Http\Requests\UpdateLinkRequest;
+use App\Models\Link;
+
+class LinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $page = request()->input('page', 1);
+
+        return Link::paginate(20, ['*'], 'page', $page)->toResourceCollection();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLinkRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Link $link)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLinkRequest $request, Link $link)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Link $link)
+    {
+        //
+    }
+}
---
+++ app/Http/Resources/LinkResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LinkResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'         => $this->id_link,
+            'link'       => $this->original_url,
+            'short'      => $this->_url,
+            'expires_at' => $this->expires_at,
+            'clicks'     => $this->clicks_count,
+            'user_id'    => $this->code_user
+        ];
+    }
+}
